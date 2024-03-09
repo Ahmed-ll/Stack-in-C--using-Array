3243d055443255ebcf897f95cde5740f1abd4793
Ahmed Ezzat <ahhmedd.ezzat@gmail.com>
2024-03-09 21:17:03 +0200

    Stack using Array

---
+++ Stack Implementation/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Stack_Implementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stack init = new Stack();
+            init.Push(5);
+            init.Push(6);
+
+            init.Pop();
+
+            init.GetTop();
+
+            init.Push(4);   
+            init.Push(9);
+
+            Console.WriteLine(init.IsEmpty());
+
+            init.Print();
+
+            Console.ReadKey();
+       
+        }
+    }
+}
---
+++ Stack Implementation/Stack.cs
@@ -0,0 +1,65 @@
+﻿using System;
+class Stack
+{
+    const int MaxSize = 10;
+    int top;
+    int[] items;
+
+    public Stack()
+    {
+        top = -1;
+        items = new int[MaxSize];
+    }
+
+    public bool IsEmpty()
+    {
+        return top < 0;
+    }
+
+    public void Push(int element)
+    {
+        if (top > MaxSize - 1)
+        {
+            Console.WriteLine("Stack is full");
+        }
+        else
+        {
+            top++;
+            items[top] = element;
+        }
+    }
+
+    public void Pop()
+    {
+        if (IsEmpty())
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        else
+        {
+            top--;
+        }
+    }
+
+    public void GetTop()
+    {
+        if (IsEmpty())
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        else
+        {
+            Console.WriteLine(items[top]);
+        }
+    }
+
+    public void Print()
+    {
+        Console.Write("[");
+        for (int i = top; i >= 0; i--)
+        {
+            Console.Write(" " + items[i] + " ");
+        }
+        Console.WriteLine("]");
+    }
+}
